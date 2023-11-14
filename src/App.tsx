4bc305f41ef5cce617304e7ec8328cd96913ccc5
Anthony Macias <Anthony40macias@gmail.com>
2023-11-13 22:24:58 -0800

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+const App =()=>  {
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Feed.tsx
@@ -0,0 +1,13 @@
+
+const Feed =()=>  {
+
+  return (
+    <>
+      <div className='feed'>
+        <h1>feed</h1>
+      </div>
+    </>
+  )
+}
+
+export default Feed;
